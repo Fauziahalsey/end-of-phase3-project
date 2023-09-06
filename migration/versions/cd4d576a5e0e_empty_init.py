cdf948618e694ca944dc0d8416dcd73c60f722bd
Fauziahalsey <fauziaomala19@gmail.com>
2023-09-06 15:55:31 +0300

    first commit

---
+++ migration/versions/cd4d576a5e0e_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: cd4d576a5e0e
+Revises: 
+Create Date: 2023-09-06 15:54:37.065505
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd4d576a5e0e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
